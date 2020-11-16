71e2bc131c22efe38ba5cd1fbbc3c94418d94ac1
Krishna Prasad <Krishnaprasadk01@gmail.com>
2020-11-16 02:29:23 +0000

    Code Changes5

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ print ("Hello Good Morning")
 print ("Have a Wonderful Day")
 print ("Always start the day with a smile")
 print ("Believe in yourself")
+print ("You are the master of your destiny")
 
