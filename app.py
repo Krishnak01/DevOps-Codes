899ed1f75b11aa233941d2dc40fe8577c0d70518
Krishna Prasad <Krishnaprasadk01@gmail.com>
2020-11-17 23:41:18 +0000

    Revert "Code Changes6"
    This reverts commit 3a1ce87c177e670f80f09df377f930c3c6c5ed8b.

--- app.py
+++ app.py
@@ -4,5 +4,4 @@ print ("Always start the day with a smile")
 print ("Believe in yourself")
 print ("You are the master of your destiny")
 print ("You can and you will")
-print ("Life begins at the end of our comfort zone")
 
